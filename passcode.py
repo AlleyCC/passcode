5f095a8d9fba54a1d444d2a22c8d49e20233567e
AlleyChang <yifanchang0602@gmail.com>
2022-03-25 12:20:31 +0800

    2nd version

--- passcode.py
+++ passcode.py
@@ -1,14 +1,12 @@
+passcode = '123456aa'
+i = 3
 while True:
-    passcode = input ('請輸入密碼:')
-    if passcode != 'a123456':
-        print ('你還有2次機會!')
-        passcode = input ('請輸入密碼:')
-    if passcode != 'a123456':
-        print ('你還有1次機會!')
-        passcode = input ('請輸入密碼:')
-    if passcode != 'a123456':
-        print ('登入失敗')
-        raise SystemExit
-    else: 
+    pwd = input ('請輸入密碼:')
+    if pwd == passcode:
+        print ('登入成功')
         break
-print ('登入成功')
+    else:
+        i = i - 1
+        print ('你還有', i ,'次機會')
+        if i < 1:
+            break
